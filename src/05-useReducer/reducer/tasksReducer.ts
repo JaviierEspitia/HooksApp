1ff4e3a14d968f3a4ff30ea318ee4d215a718c98
Javier Espitia <javi.espitiad@outlook.com>
2026-08-13 15:16:35 -0500

    adds validate to tasks form

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,26 +1,53 @@
+import * as z from 'zod';
+
 interface Todo {
   id: number;
   text: string;
   completed: boolean;
-}
+};
 
 interface TaskState {
   todos: Todo[];
   length: number;
   completed: number;
   pending: number;
-}
+};
 
 export type TaskAction = 
 | { type: 'ADD_TODO', payload: string } // recibo el string con que creo la tarea
 | { type: 'TOGGLE_TODO', payload: number } // recibo el id con que edito completo la tarea
 | { type: 'DELETE_TODO', payload: number }; // recibo el id que eliminare
 
+
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean()
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number()
+})
+
 export const getTasksInitialState = (): TaskState => {
 
   const localStorageState = localStorage.getItem('tasks-state');
 
   if(!localStorageState){
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0
+    }
+  }
+
+  const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+  
+  if( result.error ){
     return {
       todos: [],
       completed: 0,

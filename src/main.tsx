f31e40027d84eb9217d76cb05e3ab6b09bbe6d37
Javier Espitia <javi.espitiad@outlook.com>
2026-08-29 16:53:17 -0500

    implements useOptimistic

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner';
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
@@ -9,13 +10,15 @@ import { createRoot } from 'react-dom/client'
 // import { TasksApp } from './05-useReducer/TasksApp';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
 // import { MemoHook } from './06-memos/MemoHook'
-import { MemoCounter } from './06-memos/MemoCounter'
+// import { MemoCounter } from './06-memos/MemoCounter'
+import { InstagromApp } from './07-useOptimistic/InstagromApp'
 
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster/>
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect/> */}
     {/* <TrafficLightWithHook/> */}
@@ -24,6 +27,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TasksApp/> */}
     {/* {<ScrambleWords/>} */}
     {/* { <MemoHook/> } */}
-    <MemoCounter/>
+    {/* <MemoCounter/> */}
+    <InstagromApp/>
   </StrictMode>,
 )
